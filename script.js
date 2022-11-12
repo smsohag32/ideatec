d5f891dd17cbd2fdf7b5c1816af3f4a7ef07b5e9
SM Sohag <sohagsheik32@gmail.com>
2022-11-12 21:55:33 +0600

    upload file

---
+++ script.js
@@ -0,0 +1,25 @@
+let searching = function (){
+    var input, filter, tr ,td, i, txtvalue;
+    input = document.getElementById('myInput');
+        // console.log('hello')
+    filter = input.value.toUpperCase();
+    table = document.getElementById('myTable');
+    tr = table.getElementsByTagName('tr');
+    // console.log('done')
+    for(i = 0; i < tr.length; i++){
+        console.log('2');
+        td = tr[i].getElementsByTagName('td')[0];
+        console.log('tr');
+        if(td){
+            txtvalue = td.textContent || td.innerText;
+            if(txtvalue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display =''
+                // console.log('hello');
+            }
+            else{
+                tr[i].style.display = 'none';
+                console.log('not');
+            }
+        }
+    }
+}
